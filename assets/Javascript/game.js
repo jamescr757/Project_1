ac1371bb07f28b9f2e2199c30baede8cf9103e24
Nadeem Prasla <nadeemprasla@yahoo.com>
2019-10-30 14:27:13 -0500

    added ajax for upcoming release and appended to html as cards

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,7 +1,35 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    $.ajax({
+        url: "https://api.themoviedb.org/3/movie/upcoming?api_key=b4b1a288471f47d8977ade0fc9b9be70&language=en-US&page=1",
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        renderUpcoming(response)
+    });
+
+    function renderUpcoming(response) {
+        for (var i = 0; i < 10; i++) {
+            var posterImg = response.results[i].poster_path;
+            var posterTitle = response.results[i].title;
+            var posterRelease = response.results[i].release_date;
+            var cardNum = "cardNum" + i
+            var cardDiv = `<div class="col-12-sm col-3-lg col-6-md">
+            <div class="card m-4">
+            <img src="" class="card-img-top ${cardNum} " alt="">
+            <div class="card-body">
+              <p class="card-text">${posterTitle}</p>
+              <p class="card-text">${posterRelease}</p>
+            </div>
+          </div>
+          </div>`;
+            var contentCreation = $("#mainContent").append(cardDiv);
+            $(".cardNum" + i).attr("src", "https://image.tmdb.org/t/p/w185_and_h278_bestv2/" + posterImg);
+            $(".cardNum" + i).attr("alt", posterTitle + " image");
+
+
+        }
+    }
 
 
 
-
-
-}
+});
