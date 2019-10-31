8cb16dfd2be8c74756d364f47b1efbfda9a3efef
jamescr757 <55260759+jamescr757@users.noreply.github.com>
2019-10-31 10:54:30 -0500

    removed overlapping columns. need to add right column back

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
             var posterTitle = response.results[i].title;
             var posterRelease = response.results[i].release_date;
             var cardNum = "cardNum" + i
-            var cardDiv = `<div class="col-12-sm col-3-lg col-6-md">
+            var cardDiv = `<div class="col">
             <div class="card m-4 imageLayout">
             <img src="" class="card-img-top ${cardNum} " alt="">
             <div class="card-body">
@@ -22,7 +22,7 @@ $(document).ready(function() {
             </div>
           </div>
           </div>`;
-            var contentCreation = $("#mainContent").append(cardDiv);
+            var contentCreation = $(".row.cards").append(cardDiv);
             $(".cardNum" + i).attr("src", "https://image.tmdb.org/t/p/w185_and_h278_bestv2/" + posterImg);
             $(".cardNum" + i).attr("alt", posterTitle + " image");
 
